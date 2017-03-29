29e57e7ce702062bc8a817351c1d2f83f792de1f
velislav-p <v.peychev@yahoo.com>
2017-03-29 11:00:57 +0200

    auto migrations- false

--- DevEnv Semester Project/Migrations/Configuration.cs
+++ DevEnv Semester Project/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace DevEnv_Semester_Project.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(DevEnv_Semester_Project.Models.ApplicationDbContext context)
